c005fae3b5d702fee8954726348af72b8de5b3c5
Sting Alleman <stingalleman@icloud.com>
2020-12-02 11:52:08 +0100

    add part2, day1

--- src/day1.ts
+++ src/day1.ts
@@ -4,13 +4,31 @@ const numInput = fs.readFileSync(`${process.cwd()}/input/day1`).toString();
 
 const num1 = numInput.split('\n').map((a) => parseInt(a, 10));
 const num2 = numInput.split('\n').map((a) => parseInt(a, 10));
+const num3 = numInput.split('\n').map((a) => parseInt(a, 10));
 
-num1.map((n1) => {
-  num2.map((n2) => {
-    if (n1 + n2 === 2020) {
-      console.log(n1 * n2);
-    }
+function part1() {
+  return num1.map((n1) => {
+    num2.map((n2) => {
+      if (n1 + n2 === 2020) {
+        console.log(`Answer of part 1: ${n1 * n2}`);
+      }
+    });
   });
-});
-
 // Output: 918339
+}
+
+function part2() {
+  return num1.map((n1) => {
+    num2.map((n2) => {
+      num3.map((n3) => {
+        if (n1 + n2 + n3 === 2020) {
+          console.log(`Answer of part 2: ${n1 * n2 * n3}`);
+        }
+      });
+    });
+  });
+// Output: 918339
+}
+
+part1();
+part2();
